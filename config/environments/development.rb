1010fb768f9c258c0e2d02af0c551b46b6e993e9
Itsuki Hashizume <2111140089u@ed.fuk.kindai.ac.jp>
2023-11-22 07:38:42 +0000

    Welcome pageの完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "a2e4fbfdb3ab4323b83a331e20b1dfa2.vfs.cloud9.us-east-1.amazonaws.com"
 end
